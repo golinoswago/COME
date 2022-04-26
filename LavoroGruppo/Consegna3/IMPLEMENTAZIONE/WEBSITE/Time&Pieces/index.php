cb5417383be64820a2b4b4e65e276c7e62dcd9ec
golinoswago <fabio.gola@icloud.com>
2022-04-26 11:22:50 +0200

    j

---
+++ LavoroGruppo/Consegna3/IMPLEMENTAZIONE/WEBSITE/Time&Pieces/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Miniport by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Time&Pieces Logo</a></li>
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<article id="top" class="wrapper style1">
+				<div class="container">
+					<div class="row">
+						<div class="col-4 col-5-large col-12-medium">
+							<span class="image fit"><img src="images/simo.png" alt="" /></span>
+						</div>
+						<div class="col-5 col-7-large col-12-medium">
+							<header>
+								<h1>Ciao <strong>Simone</strong>.</h1>
+							</header>
+							<p><strong>Benvenuto</strong>, gestisci i tuoi timesheet e le tue attività!</p>
+						</div>
+					</div>
+					<div class="row aln-center">
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured fa-comments"></span>
+								<h3>Gestione Attività</h3>
+							</section>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon solid featured fa-camera-retro"></span>
+								<h3>Resoconti</h3>
+							</section>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<section class="box style1">
+								<span class="icon featured fa-thumbs-up"></span>
+								<h3>Pizza e fichi</h3>
+							</section>
+						</div>
+					</div>
+				</div>
+			</article>
+
+		<!-- Contact -->
+			<article id="contact" class="wrapperfooter style4">
+				<div class="container small">
+					<footer>
+						<ul id="copyright">
+							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+				</div>
+			</article>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ LavoroGruppo/Consegna3/IMPLEMENTAZIONE/WEBSITE/Time&Pieces/login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Miniport by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/login.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Home -->
+			<article>
+				<div class="">
+					<div class="row">
+						<div class="col-6" style="background:url('images/login.jfif')">
+						</div>
+						<div class="col-6">
+							<form action="action_page.php" method="post">
+
+							  <div class="container">
+								<label for="uname"><b>Username</b></label>
+								<input type="text" placeholder="Enter Username" name="uname" required>
+
+								<label for="psw"><b>Password</b></label>
+								<input type="password" placeholder="Enter Password" name="psw" required>
+
+								<button type="submit">Login</button>
+								<label>
+								  <input type="checkbox" checked="checked" name="remember"> Remember me
+								</label>
+							  </div>
+
+							  <div class="container" style="background-color:#f1f1f1">
+								<button type="button" class="cancelbtn">Cancel</button>
+								<span class="psw">Forgot <a href="#">password?</a></span>
+							  </div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</article>
+
+		<!-- Contact -->
+			<article id="contact" class="wrapperfooter style4">
+				<div class="container small">
+					<footer>
+						<ul id="copyright">
+							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+				</div>
+			</article>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
