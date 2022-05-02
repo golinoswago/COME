8f1262b33a5a36b47574ce8d0a6bd606fea6b22e
golinoswago <fabio.gola@icloud.com>
2022-05-02 18:56:16 +0200

    f

--- LavoroGruppo/Consegna3/IMPLEMENTAZIONE/WEBSITE/Time&Pieces/sendmail.php
+++ LavoroGruppo/Consegna3/IMPLEMENTAZIONE/WEBSITE/Time&Pieces/sendmail.php
@@ -40,8 +40,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                      // Set mailer to use SMTP 
 $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
 $mail->SMTPAuth = true;               // Enable SMTP authentication 
-$mail->Username = 'golino1006@gmail.com';   // SMTP username 
-$mail->Password = 'G19ctni02!';   // SMTP password 
+$mail->Username = '';   // SMTP username 
+$mail->Password = '';   // SMTP password 
 $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
 $mail->Port = 587;                    // TCP port to connect to 
  
